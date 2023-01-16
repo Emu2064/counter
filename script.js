38c2f7b930f93030d06215e34783ab7d509be4f9
Emu2064 <jucse28.2064@gmail.com>
2023-01-17 02:15:56 +0600

    creted a new branch grade-calculator

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ function calculateGrade() {
   
   if (marks >= 80 && marks <= 100) {
     document.getElementById("grade").innerText = "A+";
+  }else if (marks<80 && marks>=70) {
+    document.getElementById("grade").innerText = "A";
+  }else if (marks<70 && marks>=60) {
+    document.getElementById("grade").innerText = "A-";
+  }else if (marks<60 && marks>=50) {
+    document.getElementById("grade").innerText = "B";
+  }else if (marks<50 && marks>=40) {
+    document.getElementById("grade").innerText = "C";
+  }else{
+    document.getElementById("grade").innerText = "You're laddu guddu";
   }
 }
 
